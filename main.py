e184bf6df49ba1a3d19178d8a92dd830ef9c6c9d
huongpx <huong.px01@gmail.com>
2021-11-21 08:48:48 +0700

    Remove unnecessary files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import glob2
 from time import time, sleep
 
+
 def transparentOverlay(src, overlay, pos=(0, 0), scale=0.2):
     """
     :param src: Input Color Background Image
@@ -21,11 +22,13 @@ def transparentOverlay(src, overlay, pos=(0, 0), scale=0.2):
             if x + i >= rows or y + j >= cols:
                 continue
             alpha = float(overlay[i][j][3] / 255.0)  # read the alpha channel
-            src[x + i][y + j] = alpha * overlay[i][j][:3] + (1 - alpha) * src[x + i][y + j]
+            src[x + i][y + j] = (
+                alpha * overlay[i][j][:3] + (1 - alpha) * src[x + i][y + j]
+            )
     return src
 
 
-def addImageWatermark(logo_name,img_name,opacity,pos=(10,10)):
+def addImageWatermark(logo_name, img_name, opacity, pos=(10, 10)):
     opacity = opacity / 100
 
     img = cv2.imread(img_name)
@@ -33,15 +36,15 @@ def addImageWatermark(logo_name,img_name,opacity,pos=(10,10)):
 
     h, w, d = img.shape
     print(img.shape)
-    #Tính toán điều chỉnh ảnh
-    cut1_h = round(abs((min(h,w) - h)/2) + 0.5)
-    cut2_h = round(abs((min(h,w) - h)/2) - 0.5)
-    cut1_w = round(abs((min(h,w)  - w)/2) + 0.5)
-    cut2_w = round(abs((min(h,w)  - w)/2) - 0.5)
-    #Cắt ảnh về hình vuông
-    img = img[cut1_h:(h-cut2_h), cut1_w:(w-cut2_w)]
-    #resize ảnh về 500x500
-    img = cv2.resize(img, (500,500))
+    # Tính toán điều chỉnh ảnh
+    cut1_h = round(abs((min(h, w) - h) / 2) + 0.5)
+    cut2_h = round(abs((min(h, w) - h) / 2) - 0.5)
+    cut1_w = round(abs((min(h, w) - w) / 2) + 0.5)
+    cut2_w = round(abs((min(h, w) - w) / 2) - 0.5)
+    # Cắt ảnh về hình vuông
+    img = img[cut1_h : (h - cut2_h), cut1_w : (w - cut2_w)]
+    # resize ảnh về 500x500
+    img = cv2.resize(img, (500, 500))
 
     tempImg = img.copy()
 
@@ -50,23 +53,23 @@ def addImageWatermark(logo_name,img_name,opacity,pos=(10,10)):
     # apply the overlay
     cv2.addWeighted(overlay, opacity, output, 1 - opacity, 0, output)
 
-    #Lưu ảnh vào thư mục Anh_dich
-    #Gắn tên và thư mục đích cho ảnh đã có logo
-    img_newname = img_name.replace('Anh_goc', 'Anh_dich')
-    #Lưu ảnh mới vào Anh_dich
+    # Lưu ảnh vào thư mục Anh_dich
+    # Gắn tên và thư mục đích cho ảnh đã có logo
+    img_newname = img_name.replace("Anh_goc", "Anh_dich")
+    # Lưu ảnh mới vào Anh_dich
     cv2.imwrite(img_newname, output)
 
-#Lấy danh sách ảnh trong thư mục Anh_goc
+
+# Lấy danh sách ảnh trong thư mục Anh_goc
 list_img_jpg = glob2.glob(".\Anh_goc\*.jpg")
 list_img_png = glob2.glob(".\Anh_goc\*.png")
 
 for img_jpg_name in list_img_jpg:
 
-    #Ghép logo vào ảnh đã resize
-    addImageWatermark('./logo.png',img_jpg_name,100,(10,10))
+    # Ghép logo vào ảnh đã resize
+    addImageWatermark("./logo.png", img_jpg_name, 100, (10, 10))
 
 for img_png_name in list_img_png:
 
-    #Ghép logo vào ảnh đã resize
-    addImageWatermark('./logo.png',img_png_name,100,(10,10))
-
+    # Ghép logo vào ảnh đã resize
+    addImageWatermark("./logo.png", img_png_name, 100, (10, 10))
